8581f4838eb0273b3c3e9f44a87acb16d55a4712
Mariana Fagundes <30813734+maarifagundes@users.noreply.github.com>
2017-09-12 21:50:17 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,18 +1,13 @@
-Import jogovelha
-import sys
-erroInicializar = False
-jogo = jogovelha.inicializar()
-if len(jogo) != 3:
-erroInicializar = True
-else:
-for linha in jogo:
-if len(linha) != 3:
-erroInicializar = True
-else:
-for elemento in linha:
-if elemento != '.':
-erroInicializar = True
-if erroInicializar:
-sys.exit(1)
-else:
-sys.exit(0)
+def inicializar():
+	tab = [ ]
+	for i in range(3):
+		linha = [ ]
+		for j in range(3):
+			linha.append("X")
+		tab.append(linha)
+	return tab
+def main( ):
+	jogo = inicializar( )
+	print (jogo)
+if __name__ == "__main__":
+	main()
